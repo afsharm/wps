a74dbc4cb8289febb277fd4132228d0355ec0b9a
af <a.m@g.c>
2016-02-15 12:59:16 +0330

    getting comments from site visitors with help of Form

--- wpsa/urls.py
+++ wpsa/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'wpsa'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<wp_id>[0-9]+)/$', views.detail, name='detail')
+	url(r'^(?P<wp_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<wp_id>[0-9]+)/addcomment/$', views.addcomment, name='addcomment')
 ]
--- wpsa/views.py
+++ wpsa/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Workplace
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Workplace, Comment
 from django.template import loader
-from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 def index(request):
 	active_wp = Workplace.objects.order_by('-start_date')[:5]
@@ -12,3 +13,9 @@ def index(request):
 def detail(request, wp_id):
 	wp = get_object_or_404(Workplace, pk=wp_id)
 	return render(request, 'wpsa/detail.html', {'wp': wp})
+
+def addcomment(request, wp_id):
+	wp = get_object_or_404(Workplace, pk=wp_id)
+	c = Comment(comment = request.POST['comment'], create_date = timezone.now(), published=1, spam=1, commenter_id=-1, workplace_id=wp_id)
+	c.save()
+	return HttpResponseRedirect(reverse('wpsa:detail', args=(wp_id)))
