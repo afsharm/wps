76f6ddd71a6f0634dd4fb1c0a429ee0296a24ccb
af <a.m@g.c>
2016-02-17 15:03:07 +0330

    some tests

--- wpsa/tests.py
+++ wpsa/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Workplace
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class WorkplaceMethodTests(TestCase):
+	def published_recently(self):
+		time = timezone.now() + datetime.timedelta(days=30)
+		self.assertEqual(1,1)
+
+def create_wp(province, city):
+	return Workplace.objects.create(province=province, city=city)
+
+class WorkplaceViewTests(TestCase):
+	def test_index_view_with_no_workplaces(self):
+		response = self.client.get(reverse('wpsa:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No Workplace is defined")
+		self.assertQuerysetEqual(response.context['active_wp'], [])
