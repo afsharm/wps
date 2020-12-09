8cfb8edbe3f6f3008fea71d89f09e413e3d1b7eb
Afshar Mohebi <afshar.mohebbi@gmail.com>
2020-12-09 17:42:32 +0330

    read db connection string from env database_url

--- wps/settings.py
+++ wps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,16 +80,8 @@ WSGI_APPLICATION = 'wps.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wps',
-        'HOST': '127.0.0.1',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'PORT': '5432',
-    }
+    'default':  dj_database_url.config(default='postgresql://postgres:123@localhost:5432/wps')
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -127,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = 'c:/users/mohebbi/wps/uploads/'
+MEDIA_ROOT = 'c:/users/user_name/wps/uploads/'
 MEDIA_URL = '/uploads/'
