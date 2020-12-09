4dd81ab0b6ad4ce05e0771c91709edba000230e4
Afshar Mohebi <afshar.mohebbi@gmail.com>
2020-12-09 18:28:32 +0330

    static file path was not likely specified

--- wps/settings.py
+++ wps/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 MEDIA_ROOT = 'c:/users/user_name/wps/uploads/'
 MEDIA_URL = '/uploads/'
