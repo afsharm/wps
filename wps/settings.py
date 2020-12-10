26a9ca855b41c34225094d766b1258d46530f118
Afshar Mohebi <afshar.mohebbi@gmail.com>
2020-12-10 14:20:09 +0330

    More try on static setting

--- wps/settings.py
+++ wps/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
